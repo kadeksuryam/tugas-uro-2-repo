a9ce28c79f91611f9131df3c25fe66f669311e60
mochfatchur <fathurmuhammad72@gmail.com>
2020-01-15 13:45:56 +0700

    penambahan fungsi turunan

--- turunan1.cpp
+++ turunan1.cpp
@@ -1,38 +1,54 @@
 #include<iostream>
-#include<array>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
+vector<int> pol1,pol2;
+int deg1=0, deg2=0;
+vector<int> f1(deg1-1);
+
+void inPoly(int pilihan){
+	if(pilihan==4){
+		cout << "Masukkan degree polinomial: \n";
+		cin >> deg1;
+		for(int i=deg1;i>=0;i--){
+			int tmp;
+			cout << "Masukkan koefisien x^" << i << "\n";
+			cin >> tmp;
+			pol1.push_back(tmp);
+		}
+		reverse(pol1.begin(), pol1.end());
+		return;
+	}
+}
+
+void turunan(vector<int> &pol1){
+	for(int i=deg1-1;i>=0;i--){
+		f1[i]=pol1[i+1]*(i+1);
+	}
+}
+
+
+void outPoly(vector<int> &source){
+	cout << source[source.size()-1] << "x^" << source.size()-1;
+	for(int i=source.size()-2;i>=1;i--){
+		if(source[i] < 0) cout << " - " << abs(source[i]) << "x^" << i;
+		else cout << " + " << abs(source[i]) <<  "x^" << i;
+	}
+	if(source[0] < 0) cout << " - " << abs(source[0]);
+	else cout << " + " << abs(source[0]);
+	cout << "\n";
+}
 
 int main(){
-	int n;
-	cout<<"Masukkan degree polynom f : ";
-	cin>>n;
-	cout<<'\n';
-	int f[n+1];
-	for(int i=n;i>=0;i--){
-		cout<<"Masukkan koefisien X^"<<i<<" : ";
-		cin>>f[i];
-	}
-	for(int i=n;i>=0;i--){
-		if(i!=0){
-			cout<<f[i]<<"X^"<<i<<" + ";
-		}else{
-			cout<<f[i]<<'\n';
-		}
-	}
-	int f1[n];
-	for(int i=n-1;i>=0;i--){
-		f1[i]=f[i+1]*(i+1);
-	}
-	
-	for(int i=n-1;i>=0;i--){
-		if(i!=0){
-			cout<<f1[i]<<"X^"<<i<<" + ";
-		}else{
-			cout<<f1[i]<<'\n';
-		}
-	}
-	
-	
-	return 0;
+	cout << "Selamat datang di";
+	/*int pil;
+	cout << "Masukkan pilihan anda: \n";
+	cout << "1. Pertambahan Polinomial		2. Pengurangan Polinomial\n";
+	cout << "3. Perkalian Polinomial		4. Turunan Polinomial\n"; 
+	cin >> pil;
+	inPoly(pil);
+//	turunan(pol1);
+//	outPoly(f1);
+	return 0; */
 }
